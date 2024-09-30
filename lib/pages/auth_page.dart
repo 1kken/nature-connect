fb47238900d6c6349fe25aba6b0a4e9c4f3f7185
1ikken <tavorearljohn@gmail.com>
2024-09-30 21:56:01 +0800

    setup smtp

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -11,6 +11,28 @@ class AuthPage extends StatefulWidget {
 
 class _AuthPageState extends State<AuthPage> {
   @override
+  void initState() {
+    super.initState();
+    _checkSession();
+  }
+
+  void _checkSession() async {
+    final Session? session = Supabase.instance.client.auth.currentSession;
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!mounted) return;
+
+    if (session != null) {
+      // If a session exists, navigate to the home page
+      context.go('/home');
+    } else {
+      // If no session exists, navigate to the auth page
+      context.go('/');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -21,21 +43,29 @@ class _AuthPageState extends State<AuthPage> {
             child: Column(
           children: [
             ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child:const Image(image:  AssetImage('assets/images/logo.jpg'),width: 120,)
+                borderRadius: BorderRadius.circular(100),
+                child: const Image(
+                  image: AssetImage('assets/images/logo.jpg'),
+                  width: 120,
+                )),
+            const SizedBox(
+              height: 50,
             ),
-            const SizedBox(height: 50,),
             ConstrainedBox(
               constraints: const BoxConstraints(maxWidth: 300, maxHeight: 400),
               child: SupaEmailAuth(
-                redirectTo: 'io.mydomain.myapp://callback',
+                redirectTo: 'io.supabase.natureconnect://login-callback/',
                 onSignInComplete: (response) {
                   if (!mounted) return;
                   context.go('/home');
                 },
                 onSignUpComplete: (response) {
-                  if (!mounted) return;
-                  context.go('/home');
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    duration: const Duration(seconds: 5),
+                    content: Text("Please check your email",style: TextStyle(color: Theme.of(context).colorScheme.primary),),
+                    backgroundColor:
+                        Theme.of(context).colorScheme.secondaryContainer,
+                  ));
                 },
                 metadataFields: [
                   MetaDataField(
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,8 +13,14 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-        return const Center(
-      child: Text("this is profile"),
-    );
+        return Scaffold(
+          // body: SupaResetPassword(onSuccess: (response){})
+          body: FloatingActionButton(
+            child: const Text("Sign out"),
+            onPressed: (){
+            Supabase.instance.client.auth.signOut();
+            context.go('/');
+          }),
+        );
   }
 }
