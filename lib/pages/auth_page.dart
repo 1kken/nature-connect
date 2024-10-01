f6fe119daf00ef194409059e1e0c4e2f19a41e6f
1ikken <tavorearljohn@gmail.com>
2024-10-01 08:21:22 +0800

    added meta datas

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -10,6 +10,8 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  bool _isSigningIn = true; // Add this variable to track sign-in state
+
   @override
   void initState() {
     super.initState();
@@ -35,54 +37,89 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("NatureConnect"),
-        ),
-        body: Center(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("NatureConnect"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
             child: Column(
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: const Image(
-                  image: AssetImage('assets/images/logo.jpg'),
-                  width: 120,
-                )),
-            const SizedBox(
-              height: 50,
-            ),
-            ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 300, maxHeight: 400),
-              child: SupaEmailAuth(
-                redirectTo: 'io.supabase.natureconnect://login-callback/',
-                onSignInComplete: (response) {
-                  if (!mounted) return;
-                  context.go('/home');
-                },
-                onSignUpComplete: (response) {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    duration: const Duration(seconds: 5),
-                    content: Text("Please check your email",style: TextStyle(color: Theme.of(context).colorScheme.primary),),
-                    backgroundColor:
-                        Theme.of(context).colorScheme.secondaryContainer,
-                  ));
-                },
-                metadataFields: [
-                  MetaDataField(
-                    prefixIcon: const Icon(Icons.person),
-                    label: 'Username',
-                    key: 'username',
-                    validator: (val) {
-                      if (val == null || val.isEmpty) {
-                        return 'Please enter something';
-                      }
-                      return null;
-                    },
+              children: [
+                const SizedBox(height: 50),
+
+                // Conditionally show or hide the image based on _isSigningIn state
+                if (_isSigningIn)
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: const Image(
+                      image: AssetImage('assets/images/logo.jpg'),
+                      width: 120,
+                    ),
                   ),
-                ],
-              ),
+
+                if (_isSigningIn) const SizedBox(height: 50),
+
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: SupaEmailAuth(
+                    redirectTo: 'io.supabase.natureconnect://login-callback/',
+                    onSignInComplete: (response) {
+                      if (!mounted) return;
+                      context.go('/home');
+                    },
+                    onSignUpComplete: (response) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        duration: const Duration(seconds: 10),
+                        content: Text(
+                          "Please check your email",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondaryContainer,
+                      ));
+                    },
+
+                    // Callback that triggers when toggling between sign-in and sign-up
+                    onToggleSignIn: (isSigningIn) {
+                      setState(() {
+                        _isSigningIn =
+                            isSigningIn; // Update the state based on the toggle
+                      });
+                    },
+
+                    metadataFields: [
+                      MetaDataField(
+                        prefixIcon: const Icon(Icons.person),
+                        label: 'Username',
+                        key: 'username',
+                        validator: (val) {
+                          if (val == null || val.isEmpty) {
+                            return 'Please enter something';
+                          }
+                          return null;
+                        },
+                      ),
+                      MetaDataField(
+                        prefixIcon: const Icon(Icons.person_2_outlined),
+                        label: 'First Name',
+                        key: 'firstname',
+                      ),
+                      MetaDataField(
+                        prefixIcon: const Icon(Icons.person_2_outlined),
+                        label: 'Last Name',
+                        key: 'lastname',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        )));
+          ),
+        ),
+      ),
+    );
   }
 }
